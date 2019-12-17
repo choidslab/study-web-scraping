b4eab98f1d15a47163c8b831ea8fef0ce37ae205
choidslab <choids.lab@gmail.com>
2019-12-17 16:43:04 +0900

    chapter3 example code

---
+++ chapter3/ex_wiki_a_link.py
@@ -0,0 +1,12 @@
+import re
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
+bsObj = BeautifulSoup(html, "html.parser")
+for link in bsObj.find("div", {"id": "bodyContent"}).findAll("a",
+                                                            href = re.compile("^(/wiki/)((?!:).)*$")):
+    if 'href' in link.attrs:
+        print(link.attrs['href'])
