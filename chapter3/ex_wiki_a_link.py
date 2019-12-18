f71e1a0cc3625b223450286b49527a2293651885
choidslab <choids.lab@gmail.com>
2019-12-18 11:09:29 +0900

    modfied code

--- chapter3/ex_wiki_a_link.py
+++ chapter3/ex_wiki_a_link.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 
 html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
 bsObj = BeautifulSoup(html, "html.parser")
-for link in bsObj.find("div", {"id": "bodyContent"}).findAll("a",
-                                                            href = re.compile("^(/wiki/)((?!:).)*$")):
+for link in bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$")):
     if 'href' in link.attrs:
         print(link.attrs['href'])
