b94a70d91f163a405da56bc7d9c391addbc21344
choidslab <choids.lab@gmail.com>
2019-12-19 14:30:34 +0900

    Selenium, pyperclip을 이용한 네이버 자동로그인 스크립트

--- web_crawler_reference/crawler_with_selenium.py
+++ web_crawler_reference/crawler_with_selenium.py
@@ -1,19 +1,34 @@
+import time
 import pyperclip
-from selenium import webdriver
 
-WEB_DRIVER_PATH = 'D:/chromedriver.exe'
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+WEB_DRIVER_PATH = 'D:/chromedriver.exe'  # Chrome web driver 파일 경로 설정
 URL = 'https://nid.naver.com/nidlogin.login'
 
-NAVER_ID = ''
-NAVER_PW = ''
+NAVER_ID = ''  # Naver ID 입력
+NAVER_PW = ''  # Naver PW 입력
 
-driver = webdriver.Chrome(WEB_DRIVER_PATH)
-# driver.implicitly_wait(3)
-driver.get(URL)
 
-# ID/PW 입력
-driver.find_element_by_name('id').send_keys(NAVER_ID)
-driver.find_element_by_name('pw').send_keys(NAVER_PW)
+def input_account_info(xpath, input):
+    pyperclip.copy(input)
+    driver.find_element_by_xpath(xpath).click()
+    ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()  # 키보드 Ctrl + v 수행
+    time.sleep(1)
 
-# Login 버튼 클릭
-driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+
+if __name__ == "__main__":
+
+    driver = webdriver.Chrome(WEB_DRIVER_PATH)
+    driver.implicitly_wait(1)
+    driver.get(URL)
+
+    # ID/PW 입력
+    # driver.find_element_by_name('id').send_keys(NAVER_ID)
+    # driver.find_element_by_name('pw').send_keys(NAVER_PW)
+
+    input_account_info('//*[@id="id"]', NAVER_ID)
+    input_account_info('//*[@id="pw"]', NAVER_PW)
+    driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
